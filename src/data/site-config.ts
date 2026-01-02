a7c2991443c722ed414206fb59e37d58b1fce008
Amund Fredriksen <amund.fredriksen@outlook.com>
2026-01-02 20:43:18 +0100

    Clean up site config file and edit Home page text.

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -42,9 +42,7 @@ const siteConfig: SiteConfig = {
     // subtitle: '',
     description: 'Portfolio website, Amund Fredriksen',
     image: {
-        // src: '/dante-preview.jpg',
         src: '/placeholder.png',
-        // alt: 'Dante - Astro.js and Tailwind CSS theme'
         alt: 'Portfolio website'
     },
     headerNavLinks: [
@@ -80,29 +78,8 @@ const siteConfig: SiteConfig = {
         //     text: 'Contact',
         //     href: '/contact'
         // }
-
-        // {
-        //     text: 'Terms',
-        //     href: '/terms'
-        // },
-        // {
-        //     text: 'Download theme',
-        //     href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        // }
     ],
     socialLinks: [
-        // {
-        //     text: 'Dribbble',
-        //     href: 'https://dribbble.com/'
-        // },
-        // {
-        //     text: 'Instagram',
-        //     href: 'https://instagram.com/'
-        // },
-        // {
-        //     text: 'X/Twitter',
-        //     href: 'https://twitter.com/'
-        // }
         {
             text: 'Contact',
             href: '/contact'
@@ -114,10 +91,8 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Hello and welcome!',
-        // text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",]
-        // text: "I'm Amund Fredriksen, someone new to the field of data analysis with a background in physics. The main purpose of this site is to have a place where I can document some of the relevant professional skills I've learned outside of my formal education. As I learn new things related to data analysis and data science, I'll once in a while do a project using what I've learned and post it on this site.",
-        text: "I have a background in physics and I'm currently developing my skills and experience in data science. The main purpose of this site is to have a place where I can showcase some relevant professional skills I've learned outside of my formal education. As I learn new things related to data science or adjacent fields, I'll once in a while do a project using what I've learned and post it on this site.",
-        //text: "I have a background in physics and I'm relatively new to data science. The main purpose of this site is to have a place where I can showcase some relevant professional skills I've learned outside of my formal education. As I learn new things related to data science or adjacent fields, I'll once in a while do a project using what I've learned and post it on this site.",
+        text: "I have a background in physics and I'm currently developing my skills in the data fields. The main purpose of this site is to have a place where I can showcase some relevant professional skills I've learned outside of my formal education. As I learn more things in data science, data engineering or adjacent fields, I'll once in a while do a project and post it on this site.",
+        //"I have a background in physics and I'm currently developing my skills and experience in data science. The main purpose of this site is to have a place where I can showcase some relevant professional skills I've learned outside of my formal education. As I learn new things related to data science or adjacent fields, I'll once in a while do a project using what I've learned and post it on this site.",
         image: {
             // src: '/hero.jpeg',
             src: '/placeholder-black-line.png',
@@ -131,7 +106,7 @@ const siteConfig: SiteConfig = {
         ]
     },
     // subscribe: {
-    //     title: 'Subscribe to Dante Newsletter',
+    //     title: 'Subscribe to ...',
     //     text: 'One update per week. All the latest posts directly in your inbox.',
     //     formUrl: '#'
     // },
